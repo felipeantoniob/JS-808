1f7293b1af443fcb5829fb0a2ee0395f8b7e57b0
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2022-06-20 15:01:52 +0800

    Change background colour

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
 
-import '../styles/App.css'
+import '../styles/App.scss'
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
